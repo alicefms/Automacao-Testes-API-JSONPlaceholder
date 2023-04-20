bd5220e81f47227a4e12b97b3a66788dd9b25f11
--replace-all <alicefms@hotmail.com>
2023-04-20 12:36:57 -0300

    Testa a criação de um post. Valida o status code, o schema e se o body da response contém os dados enviados

--- posts_test.js
+++ posts_test.js
@@ -1,17 +1,30 @@
 const joi = require('joi');
 
+
+const schemaOnePost = joi.object({
+    userId: joi.number(),
+    id: joi.number(),
+    title: joi.string(),
+    body: joi.string()
+})
+const schemaGetAllPosts = joi.array().items(schemaOnePost)
+
 Feature('posts');
 
 Scenario('Buscar todos os posts',  async ({ I }) => {
-    response = await I.sendGetRequest('/posts')
+    await I.sendGetRequest('/posts')
     I.seeResponseCodeIs(200)
-    I.seeResponseMatchesJsonSchema(joi.array().items(
-        joi.object({
-            userId: joi.number(),
-            id: joi.number(),
-            title: joi.string(),
-            body: joi.string()
+    I.seeResponseMatchesJsonSchema(schemaGetAllPosts)
+})
 
-        })
-    ))
+Scenario('Criar um post', async ({ I }) => {
+    body= {userId: 1, title: "primeiro post", body:"Testando a api"}
+    response = await I.sendPostRequest('/posts', body)
+    I.seeResponseCodeIs(201)
+    I.seeResponseMatchesJsonSchema(schemaOnePost)
+    I.seeResponseContainsJson(body)
+    
+    // response = await I.sendGetRequest('/posts/' + response.data.id)
+    // I.seeResponseContainsJson(body)
+    // I.seeResponseCodeIs(200)
 })
