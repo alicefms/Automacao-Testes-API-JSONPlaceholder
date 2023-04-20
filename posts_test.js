3b9cc999e5b9b80a00736f4eae9e78ec776aae73
--replace-all <alicefms@hotmail.com>
2023-04-20 11:57:04 -0300

    Testa buscar todos os posts. Valida status code e schema.

--- posts_test.js
+++ posts_test.js
@@ -1,5 +1,17 @@
+const joi = require('joi');
+
 Feature('posts');
 
-Scenario('test something',  ({ I }) => {
+Scenario('Buscar todos os posts',  async ({ I }) => {
+    response = await I.sendGetRequest('/posts')
+    I.seeResponseCodeIs(200)
+    I.seeResponseMatchesJsonSchema(joi.array().items(
+        joi.object({
+            userId: joi.number(),
+            id: joi.number(),
+            title: joi.string(),
+            body: joi.string()
 
-});
+        })
+    ))
+})
